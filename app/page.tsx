dec5c7a891a7f8e25e4c5e89649ea6a9e51ae28c
RajdeepDs <rajdeepds626@gmail.com>
2024-04-05 19:12:35 +0530

    implement webContainers correctly

--- app/page.tsx
+++ app/page.tsx
@@ -1,64 +1,68 @@
 "use client";
 import { useWebContainer } from "@/components/useWebContainer";
 import { files } from "@/config/projectFiles";
-import { useRef } from "react";
+import { useCallback, useEffect, useRef } from "react";
+
 export default function Home() {
   const webContainerInstance = useWebContainer();
   const textareaRef = useRef<HTMLTextAreaElement>(null);
   const iframeRef = useRef<HTMLIFrameElement>(null);
-  async function start() {
-    if (textareaRef.current) {
-      textareaRef.current.value = files["index.js"].file.contents;
-    }
-    await webContainerInstance?.mount(files);
-    if (webContainerInstance) {
+  const installDependencies = useCallback(async () => {
+    if (!webContainerInstance) return;
+
+    const installProcess = await webContainerInstance.spawn("npm", ["install"]);
+
+    installProcess.output.pipeTo(
+      new WritableStream({
+        write(data) {
+          console.log(data);
+        },
+      })
+    );
+
+    return installProcess.exit;
+  }, [webContainerInstance]);
+
+  const startDevServer = useCallback(() => {
+    if (!webContainerInstance) return;
+
+    webContainerInstance.spawn("npm", ["run", "start"]).then(() => {
+      webContainerInstance.on("server-ready", (port, url) => {
+        if (!iframeRef.current) return;
+        iframeRef.current.src = url;
+      });
+    });
+  }, [webContainerInstance]);
+
+  useEffect(() => {
+    async function initializeWebContainer() {
+      if (textareaRef.current) {
+        textareaRef.current.value = files["index.js"].file.contents;
+      }
+
+      if (!webContainerInstance) return;
+
+      await webContainerInstance.mount(files);
+
       const exitCode = await installDependencies();
       if (exitCode !== 0) {
         throw new Error("Installation failed");
       }
-    }
-    await startDevServer();
-  }
-  async function installDependencies() {
-    // Install dependencies
-    if (webContainerInstance) {
-      const installProcess = await webContainerInstance.spawn("npm", [
-        "install",
-      ]);
-      installProcess.output.pipeTo(
-        new WritableStream({
-          write(data) {
-            console.log(data);
-          },
-        })
-      );
-      // Wait for install command to exit
-      return installProcess.exit;
-    }
-  }
-  async function startDevServer() {
-    // Run `npm run start` to start the Express app
-    if (webContainerInstance) {
-      await webContainerInstance.spawn("npm", ["run", "start"]);
+
+      startDevServer();
     }
 
-    // Wait for `server-ready` event
-    webContainerInstance?.on("server-ready", (port, url) => {
-      if (!iframeRef.current) return;
-      iframeRef.current.src = url;
-    });
-  }
-
-  start();
+    initializeWebContainer();
+  }, [webContainerInstance, installDependencies, startDevServer]);
   return (
-    <div className="">
+    <div className="h-dvh">
       {webContainerInstance
         ? `WebContainer instance instantiated at ${webContainerInstance.workdir}.`
         : "WebContainer instance still booting."}
 
-      <div className="grid grid-cols-2 h-dvh">
-        <textarea ref={textareaRef} className="h-1/2 bg-black text-white" />
-        <iframe ref={iframeRef} className="h-1/2 border w-full" />
+      <div className="grid grid-cols-2 h-1/2">
+        <textarea ref={textareaRef} className="h-full bg-black text-white" />
+        <iframe ref={iframeRef} className="h-full border w-full" />
       </div>
     </div>
   );
